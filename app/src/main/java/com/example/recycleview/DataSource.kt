a95477477dd9622e9a711415824aa3930cbd528c
SUDIP_ALIVE4U <Sudipbanik42@gmail.com>
2024-09-29 14:48:35 +0600

    initial commit

---
+++ app/src/main/java/com/example/recycleview/DataSource.kt
@@ -0,0 +1,27 @@
+package com.example.recycleview
+
+object DataSource {
+
+    val user : MutableList<User> = mutableListOf(
+
+        User("sapla","flower","7 Am","daraditaccii"),
+        User("joba","flower","8 am","wait ami asii "),
+        User("sapla","flower","7 Am","daraditaccii"),
+        User("joba","flower","8 am","wait ami asii "),
+        User("sapla","flower","7 Am","daraditaccii"),
+        User("joba","flower","8 am","wait ami asii "),
+        User("sapla","flower","7 Am","daraditaccii"),
+        User("joba","flower","8 am","wait ami asii "),
+        User("sapla","flower","7 Am","daraditaccii"),
+        User("joba","flower","8 am","wait ami asii "),
+        User("sapla","flower","7 Am","daraditaccii"),
+        User("joba","flower","8 am","wait ami asii "),
+        User("sapla","flower","7 Am","daraditaccii"),
+        User("joba","flower","8 am","wait ami asii "),
+        User("sapla","flower","7 Am","daraditaccii"),
+        User("joba","flower","8 am","wait ami asii "),
+        User("sapla","flower","7 Am","daraditaccii"),
+        User("joba","flower","8 am","wait ami asii "),
+
+    )
+}
---
+++ app/src/main/java/com/example/recycleview/User.kt
@@ -0,0 +1,3 @@
+package com.example.recycleview
+
+data class User(val name: String, val bio :String, val time : String, val imageurl:String)
